ccd5aa1b5e2ea9b29b0ba99c112b331411ba1f11
Aliakbar <aliakbarsherov266@gmail.com>
2025-06-11 14:22:04 +0300

    Seyum if

---
+++ If#3.py
@@ -0,0 +1,126 @@
+# a=int(input())
+# b=int(input())
+# if a==b and b==a:
+#     print('It is a square')
+# else :
+#     print('It is not a square')
+
+
+
+# a=int(input())
+# sim=input()
+# b=int(input())
+# match sim:
+#     case '+':
+#         print(a+b)
+#     case '-':
+#         print(a-b)
+#     case '*':
+#         print(a+b)
+#     case '/':
+#         print(a//b)
+
+
+# a=int(input())
+# b=int(input())
+# if a==10 or b==10 or a+b==10:
+#     print('True')
+# else :
+#     print('False')
+
+
+# a=int(input())
+# n1=a%10
+# n2=a/10%10
+# n3=a/100%10
+# n4=a/1000
+# if n1==n4 and n2==n3:
+#     print('YES')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# n1=a%10
+# n2=a/10%10
+# n3=a/100%10
+# if a>0 and n3==0 and n2==0:
+#     print(f'{n1}')
+# elif a>0 and n3==0:
+#     print(f'{n2} {n1}')
+# elif a>0:
+#     print(f'{n3} {n2} {n1}')
+# else :
+#     print(f'{a*(-1)}')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a>=b and b>=c or a<=b and b<=c:
+#     print(f'{2*a} {2*b} {2*c}')
+# else :
+#     print(f'{-a} {-b} {-c}')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a+b==c or a+c==b or b+c==a:
+#     print('YES')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+
+# d=abs(b-a)
+# f=abs(c-a)
+
+# if d<f:
+#     print(f'b = {d}')
+# else :
+#     print(f'c = {f}')
+
+
+# a=int(input())
+# b=int(input())
+# if a>0 and b>0:
+#     print('True')
+# elif a<0 and b<0:
+#     print('True')
+# else :
+#     print('False')
+
+# a=int(input())
+# b=int(input())
+# if a==10 or b==10:
+#     print('YES')
+# elif a+b>=10:
+#     print('NO')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# s=a*3600
+# d=b*60
+# if s>d and s>c:
+#     print(f'{s}')
+# elif d>s and d>c:
+#     print(f'{d}')
+# else :
+#     print(f'c')
+
+
+# a=int(input())
+# b=int(input())
+# s=a*1000/3600
+# if s>=b or b<=s:
+#     print(f'{a}')
+# elif s<=b or b>=s:
+#     print(f'{b}')

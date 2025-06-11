044d7cb2af6558b5d0aaf551f6bf36f989759f46
Aliakbar <aliakbarsherov266@gmail.com>
2025-06-11 12:47:32 +0300

    Yakim if

---
+++ If#1.py
@@ -0,0 +1,165 @@
+
+# a=int(input())
+# b=int(input())
+# if a>b:
+#     print(f'{a}')
+# else :
+#     print(f'{b}')
+
+
+# a=int(input())
+# if a>0:
+#     print('1')
+# elif a<0:
+#     print('-1')
+# else :
+#     print('0')
+
+
+# a=int(input())
+# b=int(input())
+# if a>b:
+#     print('1')
+# elif a<b:
+#     print('2')
+# else :
+#     print('0')
+
+
+# a=int(input())
+# if a>0:
+#     print('Positive')
+# elif a<0:
+#         print('Negative')
+# elif a==0:
+#             print('Zero')
+
+
+# a=int(input())
+# if a>0:
+#     print(f'{a*-1}')
+# elif a<0:
+#     print(f'{a*-1}')
+# else :
+#     print(f'{0}')
+
+
+
+# a=int(input())
+# b=int(input())
+# if a%b==0:
+#     print(f'{a//b}')
+# elif b%a==0:
+#     print(f'{b//a}')
+# else :
+#     print('None of them is divisible by other!')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+
+# pos=0
+# neg=0
+# zero=0
+
+# if a>0: 
+#     pos+=1
+# if b>0:
+#     pos+=1
+# if c>0:
+#     pos+=1
+# print(f'Positive = {pos}')
+
+# if a<0:
+#     neg+=1
+# if b<0:
+#     neg+=1
+# if c<0:
+#     neg+=1 
+# print(f'Negative = {neg}')
+
+# if a==0:
+#     zero+=1
+# if b==0:
+#     zero+=1
+# if c==0:
+#     zero+=1    
+# print(f'Zero = {zero}')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+
+# if a!=b and a!=c and a!=d:
+#     print('1')
+# elif b!=a and b!=c and b!=d:
+#     print('2')
+# elif c!=a and c!=b and c!=d:
+#     print('3')
+# else :
+#     print('4')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+
+# if a==b and b==c:
+#     print('3')
+# elif a==b or b==c or c==a:
+#     print('2')
+# else :
+#     print('0')
+
+
+# a=int(input())
+# if a%3==0 and a%5==0:
+#     print('Hello World')
+# elif a%3==0:
+#     print('Hello')
+# elif a%5==0:
+#     print('World')
+
+# else :
+#     print('Khato')
+
+
+# n=int(input())
+
+# if n>0 and n%2==0:
+#     print('positive and even')
+# elif n<0 and n%2!=0:
+#     print('negative and odd')
+# elif n>0 and n%2==0:
+#     print('positive and odd')
+# elif n<0 and n%2!=0:
+#     print ('negative and even')
+
+
+# a=int(input())
+# b=int(input())
+# if a%b==0 or b%a==0:
+#     print('YES')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# if a==1 or a==2 or a==4 or a==7:
+#     print('NO')
+# else :
+#     print('YES')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a%2==0 and b%2==0 and c%2==0:
+#     print('NO')
+# elif a%2!=0 and b%2!=0 and c%2!=0:
+#     print('NO')
+# else :
+#     print('YES')

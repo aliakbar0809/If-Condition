b4461da403954bfa57563facb2b17b4e93dc5a5c
Aliakbar <aliakbarsherov266@gmail.com>
2025-06-11 16:05:39 +0300

    Panjum if

---
+++ If#5.py
@@ -0,0 +1,100 @@
+# a=int(input())
+# a1=a/100
+# a2=a/10%10
+# a3=a%10
+# if a3==a1:
+#     print('YES')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# a1=a/100
+# a2=a/10%10
+# a3=a%10
+# if a1>a2 and a1>a3:
+#     print('First')
+
+# elif a2>a1 and a2>a3:
+#     print('Second')
+# else :
+#     print('Third')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a<b and b<c:
+#     print('YES')
+# else :
+#     print('NO')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a!=b and a!=c:
+#     print('1')
+# elif b!=a and b!=c:
+#     print('2')
+# else :
+#     print('3')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+# if a!=b and a!=c and a!=d:
+#     print('1')
+# elif b!=a and b!=c and b!=d:
+#     print('2')
+# elif c!=a and c!=b and c!=d:
+#     print('3')
+# else :
+#     print('4')
+
+
+# a=int(input())
+# if a==12 or a==1 or a==2:
+#     print('Winter')
+# elif a==3 or a==4 or a==5:
+#     print('Spring')
+# elif a==6 or a==7 or a==8:
+#     print('Summer')
+# else :
+#     print('Autumn')
+
+
+# a=int(input())
+# a1=a+1
+# a2=a-2
+# a3=10
+# if a>0:
+#     print(f'{a1}')
+# elif a<0:
+#     print(f'{a2}')
+# else :
+#     print(f'{a3}')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a<b and a>c:
+#     print(f'{a}')
+# elif b<a and b>c:
+#     print(f'{b}')
+# else :
+#     print(f'{c}')
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a<=b and a<=c:
+#     print(f'{b+c}')
+# elif b<=a and b<=c:
+#     print(f'{a+c}')
+# else :
+#     print(f'{a+b}')
